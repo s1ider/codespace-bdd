b31699522acc2386ba93b06448e6e1ef46843e91
Denis Zvezdov <s1iderorama@gmail.com>
2017-07-15 12:03:23 +0300

    switched to chromedriver

--- features/environment.py
+++ features/environment.py
@@ -14,22 +14,22 @@ def before_all(context):
         context.settings = yaml.load(f.read())
 
     # setup webdriver remotely
-    context.logger.debug("setting up WebDriver")
-    if os.path.exists('browser.session') and context.config.userdata.get('reuse', False):
-        with open('browser.session') as f:
-            session_id, session_url = f.read().split(' ')
-        context.browser = webdriver.Remote(session_url, 
-            desired_capabilities=webdriver.DesiredCapabilities.CHROME)
-        context.browser.close()
-        context.browser.session_id = session_id
-    else:
-        context.browser = webdriver.Remote('http://127.0.0.1:4444/wd/hub/',
-            desired_capabilities=webdriver.DesiredCapabilities.CHROME)
-        with open('browser.session', 'w') as f:
-            f.write("{0} {1}".format(context.browser.session_id,
-                                     context.browser.command_executor._url))
+    # context.logger.debug("setting up WebDriver")
+    # if os.path.exists('browser.session') and context.config.userdata.get('reuse', False):
+    #     with open('browser.session') as f:
+    #         session_id, session_url = f.read().split(' ')
+    #     context.browser = webdriver.Remote(session_url, 
+    #         desired_capabilities=webdriver.DesiredCapabilities.CHROME)
+    #     context.browser.close()
+    #     context.browser.session_id = session_id
+    # else:
+    #     context.browser = webdriver.Remote('http://127.0.0.1:4444/wd/hub/',
+    #         desired_capabilities=webdriver.DesiredCapabilities.CHROME)
+    #     with open('browser.session', 'w') as f:
+    #         f.write("{0} {1}".format(context.browser.session_id,
+    #                                  context.browser.command_executor._url))
 
-    # context.browser = webdriver.Chrome()
+    context.browser = webdriver.Chrome()
     context.browser.implicitly_wait(1)
 
 
